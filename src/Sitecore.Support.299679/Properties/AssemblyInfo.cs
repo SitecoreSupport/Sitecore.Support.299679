68c4cb61006b737bd9e71761e84b83fc1bb97684
Paul Stupka <paul.stupka@sitecore.com>
2019-09-29 17:23:42 +0300

    remove all unnecessary changes without chaning the behavior

--- src/Sitecore.Support.299679/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.299679/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.299679")]
 [assembly: AssemblyProduct("Sitecore.Support.299679")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Azure Search provider may delete all item-specific documents instead of a document for a specific item version")]
+
